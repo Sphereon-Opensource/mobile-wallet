d2aed96c0f2c55a8488c00cd793ee72e2c083fd7
sanderPostma <spostma@sphereon.com>
2023-01-12 13:15:15 +0100

    WAL-378: version bump

--- android/sign.cmd
+++ android/sign.cmd
@@ -1,1 +1,1 @@
-jarsigner -keystore .\eburger-play-key\upload-keystore.jks .\app\build\outputs\bundle\release\app-release.aab eburger-upload-keystore
+jarsigner -keystore .\sphereon-play-key\upload-keystore.jks .\app\build\outputs\bundle\release\app-release.aab eburger-upload-keystore
