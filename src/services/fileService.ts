e758ca0136291d8cbc95911451b47a519ac4166e
A.G.J. Cate <brummos@gmail.com>
2023-01-28 13:56:23 +0100

    WAL-371 cleanup

--- src/services/fileService.ts
+++ src/services/fileService.ts
@@ -1,6 +1,6 @@
 import * as RNFS from 'react-native-fs'
 
-import { IReadFileArgs } from '../@types/services'
+import { IReadFileArgs } from '../@types'
 
 export const readFile = async ({ filePath }: IReadFileArgs): Promise<string> => {
   return RNFS.readFile(filePath)
