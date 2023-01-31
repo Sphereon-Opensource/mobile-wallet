9964ac25e4613b8d8b9bcebef0bab86067fcc457
A.G.J. Cate <brummos@gmail.com>
2023-01-31 22:19:59 +0100

    WAL-372 cleanup

--- src/@types/navigation/index.ts
+++ src/@types/navigation/index.ts
@@ -128,7 +128,6 @@ export enum NavigationBarRoutesEnum {
 
 export enum ScreenRoutesEnum {
   WELCOME = 'Welcome',
-  PERSONAL_DATA = 'PersonalData',
   CREDENTIALS_OVERVIEW = 'CredentialsOverview',
   CREDENTIAL_DETAILS = 'CredentialDetails',
   CREDENTIAL_RAW_JSON = 'CredentialRawJson',
