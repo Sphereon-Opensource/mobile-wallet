3bfb79ae318e16421abe36753bdab2509f67df10
hrehman-sphereon <hrehman@sphereon.com>
2023-02-22 13:22:03 +0100

    [WAL-429] remove delete button

--- src/components/views/SSISwipeRowViewItem/index.tsx
+++ src/components/views/SSISwipeRowViewItem/index.tsx
@@ -12,7 +12,7 @@ export interface IProps {
   viewItem: JSX.Element
   listIndex: number
   onPress: () => Promise<void>
-  onDelete?: () => Promise<void> // In WAL-410 remove disableLeftSwipe attribute from the following line.
+  onDelete?: () => Promise<void> // In WAL-410 make it mandatory
 }
 
 const SSISwipeRowViewItem: FC<IProps> = React.forwardRef((props: IProps, ref: ForwardedRef<unknown>): JSX.Element => {
