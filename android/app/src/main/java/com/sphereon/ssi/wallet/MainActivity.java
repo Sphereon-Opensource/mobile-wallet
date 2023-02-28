26a82224bc1c50a71c19e52a0f48a4d45e4d8f2c
nklomp <nklomp@sphereon.com>
2023-02-28 18:23:17 +0100

    feat: Upgrade to expo 48

--- android/app/src/main/java/com/sphereon/ssi/wallet/MainActivity.java
+++ android/app/src/main/java/com/sphereon/ssi/wallet/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 import expo.modules.ReactActivityDelegateWrapper;
 import expo.modules.splashscreen.singletons.SplashScreen;
@@ -33,6 +35,23 @@ public class MainActivity extends ReactActivity {
   }
 
   /**
+   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
+   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+   * (aka React 18) with two boolean flags.
+   */
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+            this,
+            getMainComponentName(),
+            // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+            DefaultNewArchitectureEntryPoint.getFabricEnabled(), // fabricEnabled
+            // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
+            DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
+    );
+  }
+
+  /**
    * Align the back button behavior with Android S
    * where moving root activities to background instead of finishing activities.
    * @see <a href="https://developer.android.com/reference/android/app/Activity#onBackPressed()">onBackPressed</a>
@@ -52,24 +71,5 @@ public class MainActivity extends ReactActivity {
     super.invokeDefaultOnBackPressed();
   }
 
-  /**
-     * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
-     * you can specify the rendered you wish to use (Fabric or the older renderer).
-     */
-    @Override
-    protected ReactActivityDelegate createReactActivityDelegate() {
-      return new MainActivityDelegate(this, getMainComponentName());
-    }
-    public static class MainActivityDelegate extends ReactActivityDelegate {
-      public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
-        super(activity, mainComponentName);
-      }
-      @Override
-      protected ReactRootView createRootView() {
-        ReactRootView reactRootView = new ReactRootView(getContext());
-        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
-        return reactRootView;
-      }
-    }
+
 }
