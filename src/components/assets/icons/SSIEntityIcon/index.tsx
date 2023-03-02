9e048358a1ff9a3ed4ad06ae043188e2e501ec79
hrehman-sphereon <hrehman@sphereon.com>
2023-03-02 14:53:39 +0100

    [WAL-432] Name initials

--- src/components/assets/icons/SSIEntityIcon/index.tsx
+++ src/components/assets/icons/SSIEntityIcon/index.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from 'react'
-import {ColorValue, Text, View} from 'react-native'
+import { ColorValue, Text } from 'react-native'
 import { connect } from 'react-redux'
 
 import { entities, fonts } from '../../../../styles/colors'
+import { SSIEntityIconStyled as Avatar } from '../../../../styles/components'
 import { RootState } from '../../../../types'
 import { getGeneralInitialsOfUserName } from '../../../../utils/UserUtils'
-
 
 export interface IProps {
   size?: number
@@ -18,16 +18,15 @@ const SSIEntityIcon: FC<IProps> = (props: IProps): JSX.Element => {
   const { size = 34, fontColor = fonts.light, bgColor = entities['100'], initialsOfUserName = '?' } = props
 
   return (
-    <View style={{
-      alignItems: 'center',
-      justifyContent: 'center',
-      aspectRatio: 1,
-      borderRadius: 17,
-      backgroundColor: bgColor,
-      width: size
-    }}>
-      <Text  style={{color: fontColor}}>{initialsOfUserName}</Text>
-    </View>
+    <Avatar
+      style={{
+        marginTop: 5,
+        backgroundColor: bgColor,
+        width: size
+      }}
+    >
+      <Text style={{ color: fontColor }}>{initialsOfUserName}</Text>
+    </Avatar>
   )
 }
 
---
+++ src/styles/components/components/SSIEntityIcon/index.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native'
+
+export const SSIEntityIconStyled = styled.View`
+ align-items: center;
+ justify-content: center;
+ aspect-ratio: 1;
+ border-radius: 17;
+`
