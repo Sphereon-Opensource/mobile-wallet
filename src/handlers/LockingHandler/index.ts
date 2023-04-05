38439eb7469ac06fc2b5ab81c2d96529e1b0ec00
hrehman-sphereon <hrehman@sphereon.com>
2023-04-05 15:40:47 +0200

    [WAL-481] improve PR.

--- src/handlers/LockingHandler/index.ts
+++ src/handlers/LockingHandler/index.ts
@@ -5,18 +5,25 @@ import {APP_ID} from '../../@config/constants';
 const debug = Debug(`${APP_ID}:IntentHandler`);
 import {AppState} from 'react-native';
 import {NativeEventSubscription} from 'react-native/Libraries/EventEmitter/RCTNativeAppEventEmitter';
+import RootNavigation from '../../navigation/rootNavigation';
 
 import store from '../../store';
 import {logout} from '../../store/actions/user.actions';
+import {ScreenRoutesEnum} from "../../types";
 
 class LockingHandler {
   private lockingEventListener: NativeEventSubscription;
 
   public enableLocking = async (): Promise<void> => {
     const handleAppStateChange = async (nextAppState: string): Promise<void> => {
+
+      console.log("something: " + RootNavigation.getCurrentRoute());
+
       if (nextAppState === 'background' || nextAppState === 'active') {
-        debug('Locking application...');
-        await store.dispatch<any>(logout());
+        if (ScreenRoutesEnum.QR_READER !== RootNavigation.getCurrentRoute()) {
+          debug('Locking application...');
+          await store.dispatch<any>(logout());
+        }
       }
     };
 
