34bb279be76ad58e8f0586b4d5281b43b63a02e4
A.G.J. Cate <brummos@gmail.com>
2023-04-27 13:22:13 +0200

    WAL-535 added types

--- src/utils/NavigationUtils.ts
+++ src/utils/NavigationUtils.ts
@@ -1,31 +1,32 @@
 import {ScreenRoutesEnum} from '../types'
 import {NativeStackNavigationProp} from '@react-navigation/native-stack'
 import {navigationRef} from '../navigation/rootNavigation'
-import {CommonActions} from '@react-navigation/routers'
+import {CommonActions, NavigationState } from '@react-navigation/routers'
+import {Route} from '@react-navigation/native';
 
 /**
- * Filters routes from the QR navigation stack
+ * Filters routes from a navigation stack
  * @param navigation The navigation object
  * @param filter Routes to be filtered
  */
 // FIXME currently we only look for the QRStack, this should become a param so we can define which stack we want to work with
 export const filterNavigationStack = (navigation: NativeStackNavigationProp<any>, filter: Array<ScreenRoutesEnum>) => {
-  const rootState = navigationRef.current?.getRootState();
+  const rootState: NavigationState | undefined = navigationRef.current?.getRootState();
   if (!rootState) {
     return
   }
 
   // TODO we need to make this a param
-  const MainStack = rootState!.routes!.find((route: any) => route.name === 'Main')!.state
-  const HomeStack = MainStack!.routes!.find((route: any) => route.name === 'Home').state
-  const QRStack = HomeStack.routes.find((route: any) => route.name === 'QRStack').state
+  const MainStack = rootState!.routes!.find((route: Route<string>) => route.name === 'Main')!.state
+  const HomeStack = MainStack!.routes!.find((route: Route<string>) => route.name === 'Home').state
+  const QRStack = HomeStack.routes.find((route: Route<string>) => route.name === 'QRStack').state
 
-  const filteredRoutes = QRStack.routes.filter((route: any) => !filter.includes(route.name))
+  const filteredRoutes = QRStack.routes.filter((route: Route<ScreenRoutesEnum>) => !filter.includes(route.name))
 
   navigation.dispatch(
     CommonActions.reset({
       index: filteredRoutes.length,
-      routes: filteredRoutes.map((route: any) => ({ name: route.name, params: route.params }))
+      routes: filteredRoutes.map((route: Route<string>) => ({ name: route.name, params: route.params }))
     }),
   );
 };
