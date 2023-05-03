b05104e0b81a68442430b37acc44a227e8f6dc85
sksadjad <skhoshkhou@sphereon.com>
2023-05-03 16:34:52 +0200

    refactor: in SSICredentialRequiredViewItem made the creation of IconContainer determined by isMatching prop

--- src/components/views/SSICredentialRequiredViewItem/index.tsx
+++ src/components/views/SSICredentialRequiredViewItem/index.tsx
@@ -36,11 +36,11 @@ const SSICredentialRequiredViewItem: FC<Props> = React.forwardRef((props: Props,
 
   return (
     <Container key={id} style={{backgroundColor: listIndex % 2 == 0 ? backgrounds.secondaryDark : backgrounds.primaryDark}} onPress={onPress}>
-      <ContentContainer style={{...(!isMatching && {marginLeft: 29})}}>
+      <ContentContainer>
         {isMatching && <IconContainer>
           <SSICheckmarkIcon color={statuses.valid}/>
         </IconContainer>}
-        <ContentContainer>
+        <ContentContainer style={{...(!isMatching && {marginLeft: 29})}}>
           <View>
             <CredentialTitleCaption>{title}</CredentialTitleCaption>
             {selected.length > 0 ? (
