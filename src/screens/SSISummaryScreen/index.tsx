f9e18a3dedf3b518144c733c2b0f8c20d7ae81e1
sksadjad <skhoshkhou@sphereon.com>
2023-05-15 10:31:33 +0200

    refactor: moved static style to a new defined container

--- src/screens/SSISummaryScreen/index.tsx
+++ src/screens/SSISummaryScreen/index.tsx
@@ -7,8 +7,7 @@ import SSICredentialDetailsView from '../../components/views/SSICredentialDetail
 import SSITabView from '../../components/views/SSITabView';
 import {translate} from '../../localization/Localization';
 import {finalizeOnboarding} from '../../store/actions/onboarding.actions';
-import {backgrounds} from "../../styles/colors";
-import {SSIBasicHorizontalCenterContainerStyled as Container} from '../../styles/components';
+import {SSISummaryScreenContainerStyled as Container} from '../../styles/components/components/SSISummaryScreenContainer';
 import {ICredentialDetailsRow, ITabViewRoute, RootState, ScreenRoutesEnum, StackParamList} from '../../types';
 import {IOnboardingState} from '../../types/store/onboarding.types';
 
@@ -66,7 +65,7 @@ class SSIOnboardingSummaryScreen extends PureComponent<IProps> {
     ];
 
     return (
-      <Container style={{backgroundColor:backgrounds.secondaryDark}}>
+      <Container>
         <SSITabView routes={routes}/>
         <SSIButtonsContainer
           primaryButton={{
---
+++ src/styles/components/components/SSISummaryScreenContainer/index.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+
+import {backgrounds} from "../../../colors";
+import {SSIBasicContainerStyled} from "../../containers";
+
+export const SSISummaryScreenContainerStyled = styled(SSIBasicContainerStyled)`
+  align-items: center;
+  backgroundColor: ${backgrounds.secondaryDark};
+`;
