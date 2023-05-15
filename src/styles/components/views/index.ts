2508993e9c4ff66e9968b28bce3368e50ade1b5c
sksadjad <skhoshkhou@sphereon.com>
2023-05-15 09:11:40 +0200

    refactor: removed redundant styling

--- src/styles/components/views/index.ts
+++ src/styles/components/views/index.ts
@@ -4,5 +4,4 @@ import styled from 'styled-components/native';
 // added a typing workaround as styled.FlatList does not support typing definition of styled-components
 export const SSIDetailsViewDetailsListStyled = styled.FlatList`
   margin-bottom: 12px;
-  flex:1;
 ` as unknown as typeof FlatList;
