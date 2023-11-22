ec77800696771deec1977cfe29236f307bb79bb2
A.G.J. Cate <brummos@gmail.com>
2023-11-22 12:24:17 +0100

    WAL-661 added comment

--- src/types/error/index.ts
+++ src/types/error/index.ts
@@ -1,6 +1,7 @@
 export type ErrorDetails = {
   title: string;
   message: string;
+  // TODO would be nice if we can bundle these details fields into a new type so that we can check on this field instead of the 2 separately
   detailsTitle?: string;
   detailsMessage?: string;
 };
