8f5764cf17000127a7a99043307959c27350750f
Mario Murrent <mario@murrent.at>
2023-12-15 13:47:49 +0100

    feat: Added emergency call button

--- src/screens/SSILockScreen/index.tsx
+++ src/screens/SSILockScreen/index.tsx
@@ -1,23 +1,27 @@
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import React, {FC} from 'react';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import React, { FC } from 'react';
 
-import {PIN_CODE_LENGTH} from '../../@config/constants';
+import { PIN_CODE_LENGTH } from '../../@config/constants';
 import SSIPinCode from '../../components/pinCodes/SSIPinCode';
-import {translate} from '../../localization/Localization';
-import {storageGetPin} from '../../services/storageService';
+import { translate } from '../../localization/Localization';
+import { storageGetPin } from '../../services/storageService';
 import {
   SSIBasicHorizontalCenterContainerStyled as Container,
   SSILockScreenPinCodeContainerStyled as PinCodeContainer,
   SSIStatusBarDarkModeStyled as StatusBar,
 } from '../../styles/components';
-import {ScreenRoutesEnum, StackParamList} from '../../types';
+import { ScreenRoutesEnum, StackParamList } from '../../types';
+import Share from 'react-native-share';
+import { CredentialMapper, OriginalVerifiableCredential } from '@sphereon/ssi-types';
+import SSIPrimaryButton from '../../components/buttons/SSIPrimaryButton';
+import { Agent, VCard, AgentState, Conversation, ConversationState, DEC112Specifics, EmergencyMessageType, Header } from 'ng112-js/dist/node';
 
 type Props = NativeStackScreenProps<StackParamList, ScreenRoutesEnum.LOCK>;
 
 // TODO This screen should be extended to do pin code or biometrics authentication
 const SSILockScreen: FC<Props> = (props: Props): JSX.Element => {
   const onVerification = async (value: string): Promise<void> => {
-    const {onAuthenticate} = props.route.params;
+    const { onAuthenticate } = props.route.params;
 
     // TODO We are currently only supporting a single user right now
     if (value !== (await storageGetPin())) {
@@ -26,9 +30,57 @@ const SSILockScreen: FC<Props> = (props: Props): JSX.Element => {
     await onAuthenticate();
   };
 
+  const requestEmergency = async () => {
+    console.log('REQUEST EMERGENCY');
+    try {
+      const agent = new Agent({
+        endpoint: 'wss://app.staging.dec112.eu:443',
+        domain: 'app.staging.dec112.eu',
+        user: '8ea1b78ba4db367102cfd9a41e34fb39',
+        password: 'd7c1a156b1c9e1fc27870c514dfaf25a',
+        displayName: '0043555777777777',
+        namespaceSpecifics: new DEC112Specifics(undefined, '' || '', 'de'),
+        debug: (_level: number, ...values: unknown[]): void => {
+          console.debug(values);
+        },
+        userAgentConfig: {
+          connection_recovery_max_interval: 5,
+          connection_recovery_min_interval: 2,
+          user_agent: `com.sphereon.ssi.wallet`
+        }
+      });
+      agent.addStateListener(console.info);
+      agent
+        .initialize()
+        .then(() => {
+          console.info('SIP registration successful');
+          agent?.updateVCard(new VCard().addFullName('Sphereon MDM'));
+          const conversation = agent?.createConversation('sip:144@app.staging.dec112.eu' || '', {});
+          conversation.addMessageListener(console.log);
+          conversation.addStateListener(console.log);
+          console.log("Trying to send start message");
+          const startMessage = conversation?.start({
+            text: 'Silent Call from SSI Wallet',
+            extraHeaders: [{ key: 'X-Dec112-Silent', value: 'True' }]
+          });
+          startMessage?.promise
+            .then(() => {
+              console.info('Start message sent successful');
+            })
+            .catch(console.error);
+        })
+        .catch((error: unknown) => {
+          console.error('SIP registration error', error);
+        });
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <Container>
-      <StatusBar />
+      <StatusBar/>
       <PinCodeContainer>
         <SSIPinCode
           length={PIN_CODE_LENGTH}
@@ -37,7 +89,14 @@ const SSILockScreen: FC<Props> = (props: Props): JSX.Element => {
           errorMessage={translate('pin_code_invalid_code_message')}
           onVerification={onVerification}
         />
+
       </PinCodeContainer>
+      <SSIPrimaryButton
+        caption={'Request Emergency'}
+        onPress={requestEmergency}
+        disabled={false}
+        style={{ flex: 1, height: 42, margin: 50 }}
+      />
     </Container>
   );
 };
