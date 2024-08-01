c1ba56f34d5d77541a3017e1202c668a1068f70c
Niels Klomp <nklomp@sphereon.com>
2024-08-01 02:53:52 +0200

    chore: remove not needed modules

--- src/agent/index.ts
+++ src/agent/index.ts
@@ -7,27 +7,21 @@ import {getDidJwkResolver} from '@sphereon/ssi-sdk-ext.did-resolver-jwk';
 import {LinkHandlers, LogLinkHandler} from '@sphereon/ssi-sdk.core';
 import {createAgent} from '@veramo/core';
 import {PrivateKeyStore} from '@veramo/data-store';
-import {EthrDIDProvider} from '@veramo/did-provider-ethr';
-import {getDidIonResolver, IonDIDProvider} from '@veramo/did-provider-ion';
 import {SecretBox} from '@veramo/kms-local';
 import {OrPromise} from '@veramo/utils';
 import {Resolver} from 'did-resolver';
 import {DataSource} from 'typeorm';
 import {getResolver as webDIDResolver} from 'web-did-resolver';
-import {DID_PREFIX, DIF_UNIRESOLVER_RESOLVE_URL} from '../@config/constants';
+import {DID_PREFIX} from '../@config/constants';
 import {DB_ENCRYPTION_KEY} from '../@config/database';
 import {DEFAULT_DB_CONNECTION} from '../services/databaseService';
 import {IRequiredContext, KeyManagementSystemEnum, SupportedDidMethodEnum, TAgentTypes} from '../types';
 import {createAgentPlugins} from './plugins';
 
 export const didResolver = new Resolver({
-  ...getUniResolver(SupportedDidMethodEnum.DID_ETHR, {
-    resolveUrl: DIF_UNIRESOLVER_RESOLVE_URL,
-  }),
   ...getDidEbsiResolver(),
   ...getDidKeyResolver(),
   ...webDIDResolver(),
-  ...getDidIonResolver(),
   ...getDidJwkResolver(),
   ...getDidOydResolver(),
 });
@@ -35,14 +29,7 @@ export const didResolver = new Resolver({
 export const didMethodsSupported = Object.keys(didResolver['registry']).map(method => method.toLowerCase().replace('did:', ''));
 
 export const didProviders = {
-  [`${DID_PREFIX}:${SupportedDidMethodEnum.DID_ETHR}`]: new EthrDIDProvider({
-    defaultKms: KeyManagementSystemEnum.LOCAL,
-    network: 'goerli',
-  }),
   [`${DID_PREFIX}:${SupportedDidMethodEnum.DID_KEY}`]: new SphereonKeyDidProvider({
-    defaultKms: KeyManagementSystemEnum.LOCAL,
-  }),
-  [`${DID_PREFIX}:${SupportedDidMethodEnum.DID_ION}`]: new IonDIDProvider({
     defaultKms: KeyManagementSystemEnum.LOCAL,
   }),
   [`${DID_PREFIX}:${SupportedDidMethodEnum.DID_JWK}`]: new JwkDIDProvider({
