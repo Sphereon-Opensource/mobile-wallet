1fe49c24fe00e9b23d8fb72c0fa399f94ee9012b
A.G.J. Cate <brummos@gmail.com>
2024-08-08 16:23:05 +0200

    wip dpop

--- src/providers/authentication/eIDProvider.ts
+++ src/providers/authentication/eIDProvider.ts
@@ -1,8 +1,9 @@
 import {Dispatch, SetStateAction} from 'react';
 import {addMessageListener, AusweisAuthFlow, AusweisSdkMessage, sendCommand} from '@animo-id/expo-ausweis-sdk';
 import {PARMode} from '@sphereon/oid4vci-common';
-import {OpenID4VCIClient} from '@sphereon/oid4vci-client';
-import {EIDFlowState, EIDGetAuthorizationCodeArgs, EIDHandleErrorArgs, EIDInitializeArgs, EIDProviderArgs} from '../../types';
+import {createDPoP, CreateDPoPClientOpts, getCreateDPoPOptions} from '@sphereon/oid4vc-common';
+import {AccessTokenClient, OpenID4VCIClient} from '@sphereon/oid4vci-client';
+import {EIDFlowState, EIDGetAccessTokenArgs, EIDGetAuthorizationCodeArgs, EIDHandleErrorArgs, EIDInitializeArgs, EIDProviderArgs} from '../../types';
 
 class EIDProvider {
   private readonly client: OpenID4VCIClient;
@@ -23,7 +24,7 @@ class EIDProvider {
         this.handleError(error);
       },
       onSuccess: (options): void => {
-        this.getAuthorizationToken(options).then((authorizationCode: string) => onAuthenticated(authorizationCode));
+        this.getAuthorizationToken(options).then((authorizationCode: string) => this.getAccessToken({authorizationCode}));
       },
       onInsertCard: (): void => {
         this.handleStateChange({state: 'INSERT_CARD'});
@@ -110,6 +111,69 @@ class EIDProvider {
 
       return authorizationCode;
     });
+  }
+
+  private async getAccessToken(args: EIDGetAccessTokenArgs): Promise<string | void> {
+    // TODO void
+    const {authorizationCode} = args;
+    this.handleStateChange({state: 'GETTING_ACCESS_TOKEN'});
+
+    console.log(`AUTH CODE: ${authorizationCode}`);
+
+    const metadata = await this.client.retrieveServerMetadata();
+    console.log(`METADATA: ${JSON.stringify(metadata)}`);
+
+    const alg = 'HS256';
+    const jwk = {kty: 'Ed25519', crv: 'Ed25519', x: '123', y: '123'};
+    const jwtIssuer = {alg, jwk};
+    const htm = 'POST';
+    const htu = 'https://example.com/token';
+    const nonce = 'nonce';
+    const createDPoPOpts: CreateDPoPClientOpts = {
+      jwtIssuer: jwtIssuer,
+      dPoPSigningAlgValuesSupported: [
+        'RS256',
+        'RS384',
+        'RS512',
+        'PS256',
+        'PS384',
+        'PS512',
+        'ES256',
+        'ES256K',
+        'ES384',
+        'ES512',
+        'EdDSA',
+        'Ed25519',
+        'Ed448',
+      ],
+      jwtPayloadProps: {htm, htu, nonce} as const,
+      createJwtCallback: async (dpopJwtIssuerWithContext, jwt) => {
+        return 'eyJhbGciOiJIUzI1NiIsImp3ayI6eyJrdHkiOiJFZDI1NTE5IiwiY3J2IjoiRWQyNTUxOSIsIngiOiIxMjMiLCJ5IjoiMTIzIn0sInR5cCI6ImRwb3Arand0In0.eyJodG0iOiJQT1NUIiwiaHR1IjoiaHR0cHM6Ly9leGFtcGxlLmNvbS90b2tlbiIsIm5vbmNlIjoibm9uY2UiLCJpYXQiOjE3MjIzMjcxOTQsImp0aSI6Ijk4OWNiZTc4LWI1ZTYtNDViYS1iYjMzLWQ0MGE4ZGEwZjFhYSJ9';
+      },
+    };
+    const dPoP = await createDPoP(getCreateDPoPOptions(createDPoPOpts, metadata.authorizationServerMetadata!.token_endpoint!)); // TODO
+
+    console.log(`DPOP: ${JSON.stringify(dPoP)}`);
+
+    // const accessToken = await this.client.acquireAccessToken({
+    //     code: authorizationCode,
+    //
+    // })
+
+    const accessTokenClient = new AccessTokenClient();
+
+    const accessTokenResponse = await accessTokenClient.acquireAccessToken({
+      metadata: metadata,
+      //credentialOffer: { credential_offer: credentialOfferRequestWithBaseUrl.credential_offer },
+      //pin: 'txCode',
+      code: authorizationCode,
+      codeVerifier: 'no idea',
+      redirectUri: 'https://sphereon.com',
+      // @ts-ignore // TODO why does this complain about not having this arg?
+      createDPoPOpts,
+    });
+
+    console.log(`ACCESS TOKEN: ${JSON.stringify(accessTokenResponse)}`);
   }
 }
 
