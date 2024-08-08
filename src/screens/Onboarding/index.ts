tno-ssi-lab/sphereon-mobile-wallet

Add new onboarding machine

src/screens/Onboarding/index.ts
@@ -1,0 +1,10 @@
+export {default as AcceptTermsScreen} from './SSIAcceptTermsScreen';
+export {default as EnableBiometricsScreen} from './SSIEnableBiometricsScreen';
+export {default as EnterCountryScreen} from './SSIEnterCountryScreen';
+export {default as EnterEmailScreen} from './SSIEnterEmailScreen';
+export {default as EnterNameScreen} from './SSIEnterNameScreen';
+export {default as EnterPinCodeScreen} from './SSIEnterPinCodeScreen';
+export {default as ReadTermsScreen} from './SSIReadTermsScreen';
+export {default as ShowProgressScreen} from './SSIShowProgressScreen';
+export {default as VerifyPinCodeScreen} from './SSIVerifyPinCodeScreen';
+export {default as WelcomeScreen} from './SSIWelcomeScreen';

src/styles/components/components/index.ts
@@ -1,35 +1,37 @@
-export * from './SSIPopup';
-export * from './SSIPopupModal';
-export * from './SSIPinCode';
-export * from './SSITabView';
-export * from './SSITabViewHeader';
-export * from './SSICredentialDetailsView';
-export * from './SSITextField';
-export * from './SSICredentialSelectTypeViewItem';
-export * from './SSICheckbox';
-export * from './SSIContactViewItem';
-export * from './SSICredentialViewItem';
-export * from './SSIIdentityViewItem';
-export * from './SSIIdentitiesView';
-export * from './SSIProgressIndicator';
-export * from './SSIPinCodeSegment';
+export * from './BadgeButton';
 export * from './SSIAlert';
-export * from './SSIQRCustomMarker';
-export * from './SSIHeaderBar';
-export * from './SSIConnectionDetailsView';
-export * from './SSINavigationBar';
-export * from './SSISwipeDeleteButton';
-export * from './SSISwipeRowViewItem';
-export * from './SSIStatusBar';
-export * from './SSITextInputField';
-export * from './SSIWelcomeView';
 export * from './SSIButtonsContainer';
-export * from './SSITermsOfServiceView';
+export * from './SSICheckbox';
+export * from './SSIConnectionDetailsView';
+export * from './SSIContactViewItem';
+export * from './SSICredentialDetailsView';
+export * from './SSICredentialRequiredViewItem';
+export * from './SSICredentialSelectTypeViewItem';
+export * from './SSICredentialSelectViewItem';
+export * from './SSICredentialViewItem';
 export * from './SSIDropDownList';
 export * from './SSIDropDownListItem';
-export * from './SSIProfileIcon';
-export * from './SSICredentialRequiredViewItem';
-export * from './SSICredentialSelectViewItem';
-export * from './SSIToast';
+export * from './SSIHeaderBar';
+export * from './SSIIdentitiesView';
+export * from './SSIIdentityViewItem';
 export * from './SSIImageField';
-export * from './BadgeButton';
+export * from './SSINavigationBar';
+export * from './SSIOnboardingHeader';
+export * from './SSIPinCode';
+export * from './SSIPinCodeSegment';
+export * from './SSIPopup';
+export * from './SSIPopupModal';
+export * from './SSIProfileIcon';
+export * from './SSIProgressBarIndicator';
+export * from './SSIProgressIndicator';
+export * from './SSIQRCustomMarker';
+export * from './SSIStatusBar';
+export * from './SSISwipeDeleteButton';
+export * from './SSISwipeRowViewItem';
+export * from './SSITabView';
+export * from './SSITabViewHeader';
+export * from './SSITermsOfServiceView';
+export * from './SSITextField';
+export * from './SSITextInputField';
+export * from './SSIToast';
+export * from './SSIWelcomeView';

src/types/navigation/index.ts
@@ -1,12 +1,12 @@
 import {Format, PresentationDefinitionV1, PresentationDefinitionV2} from '@sphereon/pex-models';
 import {NonPersistedIdentity, Party} from '@sphereon/ssi-sdk.data-store';
 import {OriginalVerifiableCredential} from '@sphereon/ssi-types';
+import {CredentialSummary} from '@sphereon/ui-components.credential-branding';
 import {VerifiableCredential} from '@veramo/core';
 import {IButton, PopupBadgesEnum, PopupImagesEnum} from '../component';
 import {ICredentialSelection, ICredentialTypeSelection} from '../credential';
 import {OnboardingMachineContext, OnboardingMachineInterpreter, OnboardingPersonalData} from '../machines/onboarding';
 import {SiopV2MachineInterpreter} from '../machines/siopV2';
-import {CredentialSummary} from '@sphereon/ui-components.credential-branding';
 
 export type StackParamList = {
   CredentialsOverview: Record<string, never>;
@@ -25,7 +25,18 @@ export type StackParamList = {
   ContactAdd: IContactAddProps & Partial<IHasOnBackProps>;
   Onboarding: IOnboardingProps;
   Main: Record<string, never>;
+  // Onboarding
   Welcome: IHasOnboardingContext & IHasOnNextProps;
+  ShowProgress: OnbaordingRouteParams;
+  EnterName: OnbaordingRouteParams & IEnterNameProps;
+  EnterEmailAddress: OnbaordingRouteParams;
+  EnterCountry: OnbaordingRouteParams;
+  EnterPinCode: OnbaordingRouteParams;
+  VerifyPinCode: OnbaordingRouteParams;
+  EnableBiometrics: OnbaordingRouteParams;
+  AcceptTerms: OnbaordingRouteParams;
+  ReadTerms: OnbaordingRouteParams;
+  //
   TermsOfService: IHasOnboardingContext & ITermsOfServiceProps & IHasOnBackProps & IHasOnNextProps;
   PersonalData: IHasOnboardingContext & IHasOnBackProps & IPersonalDataProps;
   PinCodeSet: IPinCodeSetProps & IHasOnboardingContext & IHasOnBackProps & IHasOnNextProps; // TODO WAL-677 also partials for IHasOnBackProps?
@@ -43,6 +54,10 @@ export type StackParamList = {
   OID4VCI: Record<string, never>;
 };
 
+export type OnbaordingRouteParams = IHasOnboardingContext & {
+  step: number;
+};
+
 export type IBrowserOpen = IHasOnBackProps &
   IHasOnNextProps & {
     headerCaptioni18n?: string;
@@ -50,13 +65,15 @@ export type IBrowserOpen = IHasOnBackProps &
     bodyTexti18n?: string;
     actionNextLabeli18n?: string;
   };
-
 interface IPersonalDataProps {
   isDisabled: (personalData: OnboardingPersonalData) => boolean;
   onNext: (personalData: OnboardingPersonalData) => void;
   onPersonalData: (personalData: OnboardingPersonalData) => void;
 }
 
+type IEnterNameProps = {
+  onChangeText: (value: string) => void;
+};
 export interface IOnboardingProps {
   customOnboardingInstance?: OnboardingMachineInterpreter;
 }
@@ -65,7 +82,13 @@ export interface IHasOnboardingContext {
   context: OnboardingMachineContext;
 }
 
+export interface IHasOnboardingContext {
+  context: OnboardingMachineContext;
+  step: number;
+}
+
 export interface IHasOnBackProps {
+  onClick: () => {};
   onBack: () => Promise<void>;
 }
 
@@ -211,6 +234,15 @@ export enum NavigationBarRoutesEnum {
 
 export enum ScreenRoutesEnum {
   WELCOME = 'Welcome',
+  SHOW_PROGRESS = 'ShowProgress',
+  ENTER_NAME = 'EnterName',
+  ENTER_EMAIL_ADDRESS = 'EnterEmailAddress',
+  ENTER_COUNTRY = 'EnterCountry',
+  ENTER_PIN_CODE = 'EnterPinCode',
+  VERIFY_PIN_CODE = 'VerifyPinCode',
+  ENABLE_BIOMETRICS = 'EnableBiometrics',
+  ACCEPT_TERMS = 'AcceptTerms',
+  READ_TERMS = 'ReadTerms',
   CREDENTIALS_OVERVIEW = 'CredentialsOverview',
   CREDENTIAL_DETAILS = 'CredentialDetails',
   CREDENTIAL_RAW_JSON = 'CredentialRawJson',

Sphereon-Opensource/mobile-wallet

src/screens/Onboarding/index.ts
@@ -1,0 +1,10 @@
+export {default as AcceptTermsScreen} from './AcceptTermsScreen';
+export {default as EnableBiometricsScreen} from './EnableBiometricsScreen';
+export {default as EnterCountryScreen} from './EnterCountryScreen';
+export {default as EnterEmailScreen} from './EnterEmailScreen';
+export {default as EnterNameScreen} from './EnterNameScreen';
+export {default as EnterPinCodeScreen} from './EnterPinCodeScreen';
+export {default as ReadTermsScreen} from './ReadTermsScreen';
+export {default as ShowProgressScreen} from './ShowProgressScreen';
+export {default as VerifyPinCodeScreen} from './VerifyPinCodeScreen';
+export {default as WelcomeScreen} from './WelcomeScreen';

src/styles/components/components/index.ts
@@ -1,35 +1,37 @@
-export * from './SSIPopup';
-export * from './SSIPopupModal';
-export * from './SSIPinCode';
-export * from './SSITabView';
-export * from './SSITabViewHeader';
-export * from './SSICredentialDetailsView';
-export * from './SSITextField';
-export * from './SSICredentialSelectTypeViewItem';
-export * from './SSICheckbox';
-export * from './SSIContactViewItem';
-export * from './SSICredentialViewItem';
-export * from './SSIIdentityViewItem';
-export * from './SSIIdentitiesView';
-export * from './SSIProgressIndicator';
-export * from './SSIPinCodeSegment';
+export * from './BadgeButton';
+export * from './OnboardingHeader';
+export * from './ProgressBarIndicator';
 export * from './SSIAlert';
-export * from './SSIQRCustomMarker';
-export * from './SSIHeaderBar';
-export * from './SSIConnectionDetailsView';
-export * from './SSINavigationBar';
-export * from './SSISwipeDeleteButton';
-export * from './SSISwipeRowViewItem';
-export * from './SSIStatusBar';
-export * from './SSITextInputField';
-export * from './SSIWelcomeView';
 export * from './SSIButtonsContainer';
-export * from './SSITermsOfServiceView';
+export * from './SSICheckbox';
+export * from './SSIConnectionDetailsView';
+export * from './SSIContactViewItem';
+export * from './SSICredentialDetailsView';
+export * from './SSICredentialRequiredViewItem';
+export * from './SSICredentialSelectTypeViewItem';
+export * from './SSICredentialSelectViewItem';
+export * from './SSICredentialViewItem';
 export * from './SSIDropDownList';
 export * from './SSIDropDownListItem';
-export * from './SSIProfileIcon';
-export * from './SSICredentialRequiredViewItem';
-export * from './SSICredentialSelectViewItem';
-export * from './SSIToast';
+export * from './SSIHeaderBar';
+export * from './SSIIdentitiesView';
+export * from './SSIIdentityViewItem';
 export * from './SSIImageField';
-export * from './BadgeButton';
+export * from './SSINavigationBar';
+export * from './SSIPinCode';
+export * from './SSIPinCodeSegment';
+export * from './SSIPopup';
+export * from './SSIPopupModal';
+export * from './SSIProfileIcon';
+export * from './SSIProgressIndicator';
+export * from './SSIQRCustomMarker';
+export * from './SSIStatusBar';
+export * from './SSISwipeDeleteButton';
+export * from './SSISwipeRowViewItem';
+export * from './SSITabView';
+export * from './SSITabViewHeader';
+export * from './SSITermsOfServiceView';
+export * from './SSITextField';
+export * from './SSITextInputField';
+export * from './SSIToast';
+export * from './SSIWelcomeView';

src/types/navigation/index.ts
@@ -1,12 +1,12 @@
 import {Format, PresentationDefinitionV1, PresentationDefinitionV2} from '@sphereon/pex-models';
 import {NonPersistedIdentity, Party} from '@sphereon/ssi-sdk.data-store';
 import {OriginalVerifiableCredential} from '@sphereon/ssi-types';
+import {CredentialSummary} from '@sphereon/ui-components.credential-branding';
 import {VerifiableCredential} from '@veramo/core';
 import {IButton, PopupBadgesEnum, PopupImagesEnum} from '../component';
 import {ICredentialSelection, ICredentialTypeSelection} from '../credential';
 import {OnboardingMachineContext, OnboardingMachineInterpreter, OnboardingPersonalData} from '../machines/onboarding';
 import {SiopV2MachineInterpreter} from '../machines/siopV2';
-import {CredentialSummary} from '@sphereon/ui-components.credential-branding';
 
 export type StackParamList = {
   CredentialsOverview: Record<string, never>;
@@ -25,7 +25,18 @@ export type StackParamList = {
   ContactAdd: IContactAddProps & Partial<IHasOnBackProps>;
   Onboarding: IOnboardingProps;
   Main: Record<string, never>;
+  // Onboarding
   Welcome: IHasOnboardingContext & IHasOnNextProps;
+  ShowProgress: OnboardingRouteParams;
+  EnterName: OnboardingRouteParams & IEnterNameProps;
+  EnterEmailAddress: OnboardingRouteParams;
+  EnterCountry: OnboardingRouteParams;
+  EnterPinCode: OnboardingRouteParams;
+  VerifyPinCode: OnboardingRouteParams;
+  EnableBiometrics: OnboardingRouteParams;
+  AcceptTerms: OnboardingRouteParams;
+  ReadTerms: OnboardingRouteParams;
+  //
   TermsOfService: IHasOnboardingContext & ITermsOfServiceProps & IHasOnBackProps & IHasOnNextProps;
   PersonalData: IHasOnboardingContext & IHasOnBackProps & IPersonalDataProps;
   PinCodeSet: IPinCodeSetProps & IHasOnboardingContext & IHasOnBackProps & IHasOnNextProps; // TODO WAL-677 also partials for IHasOnBackProps?
@@ -43,6 +54,10 @@ export type StackParamList = {
   OID4VCI: Record<string, never>;
 };
 
+export type OnboardingRouteParams = IHasOnboardingContext & {
+  step: number;
+};
+
 export type IBrowserOpen = IHasOnBackProps &
   IHasOnNextProps & {
     headerCaptioni18n?: string;
@@ -50,13 +65,15 @@ export type IBrowserOpen = IHasOnBackProps &
     bodyTexti18n?: string;
     actionNextLabeli18n?: string;
   };
-
 interface IPersonalDataProps {
   isDisabled: (personalData: OnboardingPersonalData) => boolean;
   onNext: (personalData: OnboardingPersonalData) => void;
   onPersonalData: (personalData: OnboardingPersonalData) => void;
 }
 
+type IEnterNameProps = {
+  onChangeText: (value: string) => void;
+};
 export interface IOnboardingProps {
   customOnboardingInstance?: OnboardingMachineInterpreter;
 }
@@ -65,7 +82,13 @@ export interface IHasOnboardingContext {
   context: OnboardingMachineContext;
 }
 
+export interface IHasOnboardingContext {
+  context: OnboardingMachineContext;
+  step: number;
+}
+
 export interface IHasOnBackProps {
+  onClick: () => {};
   onBack: () => Promise<void>;
 }
 
@@ -211,6 +234,15 @@ export enum NavigationBarRoutesEnum {
 
 export enum ScreenRoutesEnum {
   WELCOME = 'Welcome',
+  SHOW_PROGRESS = 'ShowProgress',
+  ENTER_NAME = 'EnterName',
+  ENTER_EMAIL_ADDRESS = 'EnterEmailAddress',
+  ENTER_COUNTRY = 'EnterCountry',
+  ENTER_PIN_CODE = 'EnterPinCode',
+  VERIFY_PIN_CODE = 'VerifyPinCode',
+  ENABLE_BIOMETRICS = 'EnableBiometrics',
+  ACCEPT_TERMS = 'AcceptTerms',
+  READ_TERMS = 'ReadTerms',
   CREDENTIALS_OVERVIEW = 'CredentialsOverview',
   CREDENTIAL_DETAILS = 'CredentialDetails',
   CREDENTIAL_RAW_JSON = 'CredentialRawJson',