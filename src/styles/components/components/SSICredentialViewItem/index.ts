3065917dc1f7cc40cb253eed62d7e904735fa637
A.G.J. Cate <brummos@gmail.com>
2024-08-12 13:02:02 +0200

    chore: reverted changes

--- src/styles/components/components/SSICredentialViewItem/index.ts
+++ src/styles/components/components/SSICredentialViewItem/index.ts
@@ -3,9 +3,8 @@ import styled from 'styled-components/native';
 import {SSIFlexDirectionRowViewStyled} from '../../containers';
 import {SSITextH3LightStyled, SSITextH5LightStyled} from '../../fonts';
 
-export const SSICredentialViewItemContainerStyled = styled(SSIFlexDirectionRowViewStyled)`
+export const SSICredentialViewItemContainerStyled = styled.View`
   padding: 16px 21px 18px 24px;
-  gap: 15px;
 `;
 
 export const SSICredentialViewItemContentTopContainerStyled = styled(SSIFlexDirectionRowViewStyled)`
