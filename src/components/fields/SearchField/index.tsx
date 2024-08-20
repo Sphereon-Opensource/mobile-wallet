5183899eb58e6f2b7ec0038528b2614c82b66c8b
A.G.J. Cate <brummos@gmail.com>
2024-08-20 10:56:16 +0200

    feat: added not yet implemented toasts to credential catalog

--- src/components/fields/SearchField/index.tsx
+++ src/components/fields/SearchField/index.tsx
@@ -1,14 +1,26 @@
-import {SSITextH2LightStyled as SearchCaption} from '@sphereon/ui-components.ssi-react-native';
 import React, {FC, ReactElement} from 'react';
+import {TouchableWithoutFeedback} from 'react-native';
+import {SSITextH2LightStyled as SearchCaption} from '@sphereon/ui-components.ssi-react-native';
 import Localization from '../../../localization/Localization';
 import {SearchFieldContainerStyled as Container, SearchFieldMagnifyingGlassIconStyled as MagnifyingGlassIcon} from '../../../styles/components';
+import {showToast} from '../../../utils';
+import {ToastTypeEnum} from '../../../types';
 
 const SearchField: FC = (): ReactElement => {
+  const showNotYetImplementedToast = async (): Promise<void> => {
+    showToast(ToastTypeEnum.TOAST_SUCCESS, {
+      message: Localization.translate('item_not_yet_available_message'),
+      showBadge: false,
+    });
+  };
+
   return (
-    <Container>
-      <MagnifyingGlassIcon />
-      <SearchCaption>{Localization.translate('search_field_default_caption')}</SearchCaption>
-    </Container>
+    <TouchableWithoutFeedback onPress={showNotYetImplementedToast}>
+      <Container>
+        <MagnifyingGlassIcon />
+        <SearchCaption>{Localization.translate('search_field_default_caption')}</SearchCaption>
+      </Container>
+    </TouchableWithoutFeedback>
   );
 };
 
