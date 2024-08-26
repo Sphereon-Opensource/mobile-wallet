57011ad6ced8db5624f9c5aa7d8bf1f44e73868f
A.G.J. Cate <brummos@gmail.com>
2024-08-26 11:06:09 +0200

    screen fixes (reverts)

--- src/screens/SSINotificationsOverviewScreen/index.tsx
+++ src/screens/SSINotificationsOverviewScreen/index.tsx
@@ -1,9 +1,11 @@
 import React, {FC} from 'react';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import SSINotificationsImage from '../../components/assets/images/SSINotificationsImage';
 import {translate} from '../../localization/Localization';
 import {
   SSIBasicContainerStyled as Container,
   SSINotificationsOverviewScreenEmptyStateContainerStyled as EmptyStateContainer,
+  SSINotificationsOverviewScreenEmptyStateImageContainerStyled as EmptyStateImageContainer,
   SSINotificationsOverviewScreenEmptyStateTitleTextStyled as TitleText,
   SSITextH4LightStyled as SubTitleText,
 } from '../../styles/components';
@@ -16,6 +18,9 @@ const SSINotificationsOverviewScreen: FC<Props> = (props: Props): JSX.Element =>
     // This currently now only shows the empty state.
     <Container>
       <EmptyStateContainer>
+        <EmptyStateImageContainer>
+          <SSINotificationsImage />
+        </EmptyStateImageContainer>
         <TitleText>{translate('notifications_overview_empty_state_title')}</TitleText>
         <SubTitleText>{translate('notifications_overview_empty_state_subtitle')}</SubTitleText>
       </EmptyStateContainer>
