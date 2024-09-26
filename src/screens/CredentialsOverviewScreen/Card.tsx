b359898079aee8ed2e36431c7b43d2e883361a6e
Odysseas Zafeiriou <odysseas@wearereasonablepeople.com>
2024-09-26 15:15:24 +0200

    cleanup; use reanimated

--- src/screens/CredentialsOverviewScreen/Card.tsx
+++ src/screens/CredentialsOverviewScreen/Card.tsx
@@ -1,30 +1,105 @@
-import {SSITextH3LightStyled} from '@sphereon/ui-components.ssi-react-native';
-import {useEffect} from 'react';
-import {TouchableOpacity, TouchableOpacityProps, ViewStyle} from 'react-native';
-import {useSharedValue} from 'react-native-reanimated';
-import {Accordion} from './Accordion';
+import {ImageAttributes} from '@sphereon/ui-components.core';
+import {CredentialSummary, getCredentialStatus, getIssuerLogo} from '@sphereon/ui-components.credential-branding';
+import {SSICredentialCardView, SSITextH4LightStyled} from '@sphereon/ui-components.ssi-react-native';
+import React from 'react';
+import {TouchableOpacity, TouchableWithoutFeedback} from 'react-native';
+import Animated, {Extrapolation, FadeInUp, FadeOutUp, SharedValue, interpolate, useDerivedValue, withTiming} from 'react-native-reanimated';
 
-export type CardConfig = {
-  title: string;
-  style?: ViewStyle;
+type Props = {
+  credential: CredentialSummary;
+  yScroll: SharedValue<number>;
+  listHeight: number;
+  index: number;
+  selected?: number;
+  cardsUnderSelected: number;
+  onSelect: () => void;
+  onViewDetailsPress: () => void;
 };
 
-type Props = CardConfig & {
-  isExpanded: boolean;
-  onPress: TouchableOpacityProps['onPress'];
+export const CARD_HEIGHT = 186;
+export const CARD_OVERLAY = 126;
+const CARD_EXPOSURE = CARD_HEIGHT - CARD_OVERLAY;
+const UNDER_SELECTED_CARD_HEIGHT = 48;
+const SELECTED_DRIFT = CARD_EXPOSURE - UNDER_SELECTED_CARD_HEIGHT;
+
+const getCredentialCardLogo = (credential: CredentialSummary): ImageAttributes | undefined => {
+  if (credential.branding?.logo?.uri || credential.branding?.logo?.dataUri) {
+    return credential.branding.logo;
+  }
+  const uri: string | undefined = getIssuerLogo(credential, credential.branding);
+  if (uri) {
+    return {uri};
+  }
 };
 
-export const Card = ({title, isExpanded, onPress, style = {}}: Props) => {
-  const _isExpanded = useSharedValue(isExpanded);
-  useEffect(() => {
-    _isExpanded.value = isExpanded;
-  }, [isExpanded]);
+const CredentialViewCard = ({credential}: {credential: CredentialSummary}) => {
+  const issuer: string = credential.issuer.alias;
+  const credentialCardLogo: ImageAttributes | undefined = getCredentialCardLogo(credential);
 
   return (
-    <TouchableOpacity onPress={onPress} style={[{borderRadius: 16}, style]}>
-      <Accordion isExpanded={_isExpanded} viewKey={`card_${title}`} style={{width: '100%'}}>
-        <SSITextH3LightStyled>{title}</SSITextH3LightStyled>
-      </Accordion>
-    </TouchableOpacity>
+    <SSICredentialCardView
+      header={{
+        credentialTitle: credential.branding?.alias,
+        credentialSubtitle: credential.branding?.description,
+        logo: credentialCardLogo,
+      }}
+      body={{
+        issuerName: issuer ?? credential.issuer.name,
+      }}
+      footer={{
+        credentialStatus: getCredentialStatus(credential),
+        expirationDate: credential.expirationDate,
+      }}
+      display={{
+        backgroundColor: credential.branding?.background?.color,
+        backgroundImage: credential.branding?.background?.image,
+        textColor: credential.branding?.text?.color,
+      }}
+    />
+  );
+};
+
+export const Card = ({credential, cardsUnderSelected, index, selected, yScroll, onSelect, onViewDetailsPress, listHeight}: Props) => {
+  const derivedYScroll = useDerivedValue(() => yScroll.value);
+  const selectedPos = useDerivedValue(() => (selected ? selected * CARD_EXPOSURE - derivedYScroll.value : 0));
+  const position = useDerivedValue(() => index * CARD_EXPOSURE - derivedYScroll.value);
+  const defaultTranslateY = useDerivedValue(() => {
+    const scrollFollowingTranslate = interpolate(derivedYScroll.value, [0, index * CARD_EXPOSURE], [0, -index * CARD_EXPOSURE], Extrapolation.CLAMP);
+    return derivedYScroll.value + scrollFollowingTranslate - index * CARD_OVERLAY;
+  });
+  const selectedYAdjustment = useDerivedValue(() => {
+    if (selected === undefined) {
+      return withTiming(0, {duration: 300});
+    }
+    const indexAfterSelected = index - selected;
+    const needsAdjustment = position.value > 0;
+    const isAfterSelected = index > selected;
+    const selectedTopOffset = Math.max(selectedPos.value, 0);
+    const value = !needsAdjustment
+      ? 0
+      : isAfterSelected
+      ? listHeight - (cardsUnderSelected + 1) * UNDER_SELECTED_CARD_HEIGHT - SELECTED_DRIFT * indexAfterSelected - selectedTopOffset
+      : -position.value;
+    return withTiming(value, {duration: 300});
+  });
+  const translateY = useDerivedValue(() => defaultTranslateY.value + selectedYAdjustment.value);
+  return (
+    <TouchableWithoutFeedback onPress={onSelect}>
+      <Animated.View
+        style={{
+          alignItems: 'center',
+          transform: [{translateY}],
+          height: CARD_HEIGHT,
+        }}>
+        <CredentialViewCard key={credential.id} credential={credential} />
+        {index === selected && (
+          <Animated.View entering={FadeInUp} exiting={FadeOutUp} style={{marginTop: 16}}>
+            <TouchableOpacity onPress={onViewDetailsPress}>
+              <SSITextH4LightStyled>Go to Details</SSITextH4LightStyled>
+            </TouchableOpacity>
+          </Animated.View>
+        )}
+      </Animated.View>
+    </TouchableWithoutFeedback>
   );
 };
--- src/screens/CredentialsOverviewScreen/CredentialsOveriewCardList.tsx
+++ src/screens/CredentialsOverviewScreen/CredentialsOveriewCardList.tsx
@@ -1,142 +1,37 @@
 import {useFocusEffect} from '@react-navigation/native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {CredentialRole} from '@sphereon/ssi-sdk.data-store';
-import {backgroundColors, CredentialStatus, ImageAttributes} from '@sphereon/ui-components.core';
-import {CredentialSummary, getCredentialStatus, getIssuerLogo} from '@sphereon/ui-components.credential-branding';
-import {SSICredentialCardView} from '@sphereon/ui-components.ssi-react-native';
+import {backgroundColors} from '@sphereon/ui-components.core';
+import {CredentialSummary} from '@sphereon/ui-components.credential-branding';
 import React, {useCallback} from 'react';
-import {TouchableOpacity, useWindowDimensions, View} from 'react-native';
+import {View} from 'react-native';
 import {connect} from 'react-redux';
 import {getVerifiableCredential} from '../../services/credentialService';
 import {setViewPreference} from '../../store/actions/user.actions';
 import {CreditOverviewStackParamsList, RootState, ScreenRoutesEnum} from '../../types';
 import {ConfigurableViewKey, ViewPreference} from '../../types/preferences';
 import {CardContainer} from './CardContainer';
-
-const mockCards: CredentialSummary[] = [
-  {
-    issuer: {
-      name: 'Issuer 1',
-      alias: 'Issuer 1',
-    },
-    credentialRole: CredentialRole.ISSUER,
-    hash: 'hash 1',
-    title: 'test 1',
-    expirationDate: 111111111111,
-    branding: {
-      alias: 'test 1',
-      description: 'test 1',
-      logo: {
-        uri: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
-      },
-      background: {
-        color: 'pink',
-      },
-      text: {
-        color: 'black',
-      },
-    },
-    credentialStatus: CredentialStatus.VALID,
-    issueDate: 111111111111,
-    properties: [],
-  },
-  {
-    issuer: {
-      name: 'Issuer 2',
-      alias: 'Issuer 2',
-    },
-    credentialRole: CredentialRole.ISSUER,
-    hash: 'hash 2',
-    title: 'test 2',
-    expirationDate: 111111111111,
-    branding: {
-      alias: 'test 2',
-      description: 'test 2',
-      logo: {
-        uri: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
-      },
-      background: {
-        color: 'yellow',
-      },
-      text: {
-        color: 'black',
-      },
-    },
-    credentialStatus: CredentialStatus.VALID,
-    issueDate: 111111111111,
-    properties: [],
-  },
-  {
-    issuer: {
-      name: 'Issuer 3',
-      alias: 'Issuer 3',
-    },
-    credentialRole: CredentialRole.ISSUER,
-    hash: 'hash 3',
-    title: 'test 3',
-    expirationDate: 111111111111,
-    branding: {
-      alias: 'test 3',
-      description: 'test 3',
-      logo: {
-        uri: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
-      },
-      background: {
-        color: 'purple',
-      },
-      text: {
-        color: 'black',
-      },
-    },
-    credentialStatus: CredentialStatus.VALID,
-    issueDate: 111111111111,
-    properties: [],
-  },
-];
+import {credentialSummaryMock} from './credentialMock';
 
 type Props = NativeStackScreenProps<CreditOverviewStackParamsList, 'Card'> & {
   verifiableCredentials: Array<CredentialSummary>;
   setViewPreference: (viewKey: ConfigurableViewKey, preference: ViewPreference) => void;
 };
 
-const getCredentialCardLogo = (credential: CredentialSummary): ImageAttributes | undefined => {
-  if (credential.branding?.logo?.uri || credential.branding?.logo?.dataUri) {
-    return credential.branding.logo;
-  }
-
-  const uri: string | undefined = getIssuerLogo(credential, credential.branding);
-  if (uri) {
-    return {uri};
-  }
-};
-
-const CredentialViewCard = ({credential, onPress}: {credential: CredentialSummary; onPress: () => Promise<void>}) => {
-  const issuer: string = credential.issuer.alias;
-  const credentialCardLogo: ImageAttributes | undefined = getCredentialCardLogo(credential);
-  return (
-    <TouchableOpacity onPress={onPress} style={{width: '100%'}}>
-      <SSICredentialCardView
-        header={{
-          credentialTitle: credential.branding?.alias,
-          credentialSubtitle: credential.branding?.description,
-          logo: credentialCardLogo,
-        }}
-        body={{
-          issuerName: issuer ?? credential.issuer.name,
-        }}
-        footer={{
-          credentialStatus: getCredentialStatus(credential),
-          expirationDate: credential.expirationDate,
-        }}
-        display={{
-          backgroundColor: credential.branding?.background?.color,
-          backgroundImage: credential.branding?.background?.image,
-          textColor: credential.branding?.text?.color,
-        }}
-      />
-    </TouchableOpacity>
-  );
-};
+const mocked: CredentialSummary[] = [
+  credentialSummaryMock('1'),
+  credentialSummaryMock('2'),
+  credentialSummaryMock('3'),
+  credentialSummaryMock('4'),
+  credentialSummaryMock('5'),
+  credentialSummaryMock('6'),
+  credentialSummaryMock('7'),
+  credentialSummaryMock('8'),
+  credentialSummaryMock('9'),
+  credentialSummaryMock('10'),
+  credentialSummaryMock('11'),
+  credentialSummaryMock('12'),
+  credentialSummaryMock('13'),
+];
 
 const CredentialsOverviewCardList = ({setViewPreference, verifiableCredentials, navigation}: Props) => {
   useFocusEffect(
@@ -155,49 +50,9 @@ const CredentialsOverviewCardList = ({setViewPreference, verifiableCredentials, 
     });
   };
 
-  const carouselWidth = useWindowDimensions().width - 48;
-
   return (
     <View style={{backgroundColor: backgroundColors.primaryDark, flex: 1, paddingHorizontal: 24, alignItems: 'center'}}>
-      {/* <Carousel
-        style={{
-          width: carouselWidth,
-          height: 240,
-          borderColor: "red",
-          borderWidth: 1,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-        width={carouselWidth - 50}
-        height={200}
-        pagingEnabled={true}
-        snapEnabled={true}
-        loop={true}
-        autoPlay={false}
-        autoPlayReverse={false}
-        data={mockCards}
-        mode='vertical-stack'
-        modeConfig={{
-          snapDirection: 'left',
-          stackInterval: 16,
-        }}
-        // customConfig={() => ({type: "negative", viewCount: mockCards.length})}
-        renderItem={({index, item}) => (
-          <CredentialViewCard
-            credential={item}
-            key={index}
-            onPress={() => onItemPress(item)}
-
-          // entering={FadeInRight.delay(
-          //   (viewCount - index) * 100,
-          // ).duration(200)}
-          />
-        )}
-      /> */}
-      {/* {mockCards.map((credential, index) => (
-        <CredentialViewCard key={index} credential={credential} onPress={() => onItemPress(credential)} />
-      ))} */}
-      <CardContainer />
+      <CardContainer credentials={mocked} onItemPressWhenSelected={onItemPress} />
     </View>
   );
 };
