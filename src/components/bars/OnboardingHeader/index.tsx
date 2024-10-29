cdd9fae039d6cfa8c082385029716a6633db8921
A.G.J. Cate <brummos@gmail.com>
2024-10-29 13:35:01 +0100

    chore: fix pid security on close modal behavior

--- src/components/bars/OnboardingHeader/index.tsx
+++ src/components/bars/OnboardingHeader/index.tsx
@@ -86,8 +86,9 @@ const OnboardingHeader: FC<HeaderBarProps> = ({title, stepConfig, onBack}: Heade
   );
 
   const showCogWheel = useMemo(() => {
-    console.log('current step', currentStep);
-    if (!currentStep) return false;
+    if (!currentStep) {
+      return false;
+    }
     return currentStep < 4 && !stepConfig;
   }, [currentStep]);
 
@@ -119,20 +120,20 @@ const OnboardingHeader: FC<HeaderBarProps> = ({title, stepConfig, onBack}: Heade
     };
   });
 
-  const onSelect = async () => {
-    try {
-      await storagePersistPIDSecurityModel(securityModel);
-    } catch (e) {
-      console.log('failed to persist PID security model');
-    }
-    if (securityModel === PIDSecurityModel.EID_DURING_PRESENTATION) {
-      onboardingInstance.send(OnboardingMachineEvents.SET_SKIP_IMPORT, {data: true});
-      closeModal();
-      return;
-    }
-    if (skipImport) {
-      onboardingInstance.send(OnboardingMachineEvents.SET_SKIP_IMPORT, {data: false});
-    }
+  const onClose = async (): Promise<void> => {
+    storagePersistPIDSecurityModel(securityModel)
+      .then((): void => {
+        if (securityModel === PIDSecurityModel.EID_DURING_PRESENTATION) {
+          onboardingInstance.send(OnboardingMachineEvents.SET_SKIP_IMPORT, {data: true});
+        }
+
+        if (securityModel === PIDSecurityModel.SECURE_ELEMENT) {
+          onboardingInstance.send(OnboardingMachineEvents.SET_SKIP_IMPORT, {data: false});
+        }
+
+        closeModal();
+      })
+      .catch(error => console.log(`Failed to persist PID security model. Error: ${error.message}`));
   };
 
   return (
@@ -218,8 +219,7 @@ const OnboardingHeader: FC<HeaderBarProps> = ({title, stepConfig, onBack}: Heade
                 selected={securityModel === PIDSecurityModel.EID_DURING_PRESENTATION}
               />
             </OptionContainer>
-
-            <PrimaryButton caption={translate('onboarding_pid_security_model_select')} onPress={() => onSelect()} />
+            <PrimaryButton caption={translate('onboarding_pid_security_model_select')} onPress={() => onClose()} />
           </SettingsModalContainer>
         </Animated.View>
       )}
