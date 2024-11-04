3a93c0625aee2879459cf25340fd814a559fefc1
sanderPostma <spostma@sphereon.com>
2024-11-04 13:18:49 +0100

    chore: fix credential title & dispatchIdentifier

--- src/agent/plugins.ts
+++ src/agent/plugins.ts
@@ -42,7 +42,6 @@ export const oid4vciHolder = new OID4VCIHolder({
   onIdentifierCreated: async (args: OnIdentifierCreatedArgs): Promise<void> => {
     const {identifier} = args;
     if (isManagedIdentifierDidResult(identifier)) {
-      console.log('dispatchIdentifier', identifier);
       await dispatchIdentifier({identifier: identifier.identifier});
     }
   },
