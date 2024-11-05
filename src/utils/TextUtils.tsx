29a5897b718e439121083da7302549228b32783d
Merveille van Eck <merveillevaneck@gmail.com>
2024-11-05 16:51:16 +0200

    added pressable url support to SSITextField for cred and contact info

--- src/utils/TextUtils.tsx
+++ src/utils/TextUtils.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Text} from 'react-native';
+import {URL_VALIDATION_REGEX} from '../@config/constants';
 
 const {v4: uuidv4} = require('uuid');
 
@@ -23,4 +24,19 @@ export const parseTextToElement = (text: string): JSX.Element => {
 export const capitalize = (text: string): string => {
   const lowerCaseText = text.toLowerCase();
   return lowerCaseText.charAt(0).toUpperCase() + lowerCaseText.slice(1);
+};
+
+const checkAndAddHTTPPrefix = (url: string) => {
+  if (!url.includes('http')) return 'https://' + url;
+  return url;
+};
+
+export const parseValidURL = (uri: string): false | URL => {
+  try {
+    const isMatch = URL_VALIDATION_REGEX.test(uri);
+    if (!isMatch) return false;
+    const url = new URL(checkAndAddHTTPPrefix(uri));
+    return url;
+  } catch (e) {}
+  return false;
 };
