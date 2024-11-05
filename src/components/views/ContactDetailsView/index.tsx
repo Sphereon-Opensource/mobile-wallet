cc2e1cb97a0f2e1e4ceae3d7e88d6017b910a753
Merveille van Eck <merveillevaneck@gmail.com>
2024-11-05 12:28:08 +0200

    added style prop to ContactDetailsView container

--- src/components/views/ContactDetailsView/index.tsx
+++ src/components/views/ContactDetailsView/index.tsx
@@ -1,6 +1,6 @@
 import {CredentialDetailsRow} from '@sphereon/ui-components.credential-branding';
 import {FC} from 'react';
-import {ListRenderItemInfo} from 'react-native';
+import {ListRenderItemInfo, StyleProp, ViewStyle} from 'react-native';
 import SSIImageField from '../../fields/SSIImageField';
 import SSITextField from '../../fields/SSITextField';
 import {SSICredentialDetailsViewContainerStyled as Container, SSIDetailsViewDetailsListStyled as DetailsList} from '../../../styles/components';
@@ -8,9 +8,11 @@ import {DETAILS_INITIAL_NUMBER_TO_RENDER} from '../../../@config/constants';
 
 export interface IContactDetailsViewProps {
   properties: Array<CredentialDetailsRow>;
+  style?: StyleProp<ViewStyle>;
 }
 
 export const ContactDetailsView: FC<IContactDetailsViewProps> = (props: IContactDetailsViewProps): JSX.Element => {
+  const {style, properties} = props;
   const renderItem = (itemInfo: ListRenderItemInfo<CredentialDetailsRow>) => {
     if (itemInfo.item.imageSize) {
       return <SSIImageField item={itemInfo.item} index={itemInfo.index} />;
@@ -20,9 +22,9 @@ export const ContactDetailsView: FC<IContactDetailsViewProps> = (props: IContact
   };
 
   return (
-    <Container>
+    <Container style={style}>
       <DetailsList
-        data={props.properties.filter(p => p.value !== undefined)}
+        data={properties.filter(p => p.value !== undefined)}
         renderItem={renderItem}
         keyExtractor={(item: CredentialDetailsRow) => item.id}
         initialNumToRender={DETAILS_INITIAL_NUMBER_TO_RENDER}
