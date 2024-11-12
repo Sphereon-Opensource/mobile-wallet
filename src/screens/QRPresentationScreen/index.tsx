697674fee5004c6c369b87a0a5fa19431f351412
A.G.J. Cate <brummos@gmail.com>
2024-11-12 12:11:27 +0100

    chore: refactored qr functionality to qr component

--- src/screens/QRPresentationScreen/index.tsx
+++ src/screens/QRPresentationScreen/index.tsx
@@ -1,49 +1,22 @@
-import {FC, useEffect, useState, ReactElement} from 'react';
+import {FC} from 'react';
 import {Dimensions} from 'react-native';
-import {SSIBasicContainerSecondaryStyled as SSIContainer} from '../../styles/components';
-import {QRContainer, QRPresentationViewContainer} from 'src/styles/components/screens/QRPresentationScreen';
-import {CreateElementArgs, QRType} from '@sphereon/ssi-sdk.qr-code-generator';
-import {agentContext} from '../../agent';
-import {backgroundColors, fontColors} from '@sphereon/ui-components.core';
+import {QRContainer, QRPresentationViewContainer, SSIBasicContainerSecondaryStyled as SSIContainer} from '../../styles/components';
+
+import QRCode from '../../components/qrCodes/QRCode';
 
 const {width} = Dimensions.get('screen');
 
-/* FIXME: Replace this with some result once back end for holder
- * presentations is complete.
- */
-const mockURI = 'some_uri_content';
-
 const QRPresentationScreen: FC = () => {
-  const [qrElement, setQrElement] = useState<ReactElement | null>(null);
-  const delegate = async () => {
-    const uriElementArgs: CreateElementArgs<QRType.URI, string> = {
-      data: {
-        type: QRType.URI,
-        object: mockURI,
-        //FIXME: determine if the id field should be necessary on a URI
-        //element
-        id: '',
-      },
-      renderingProps: {
-        bgColor: backgroundColors.primaryLight,
-        fgColor: fontColors.dark,
-        level: 'Q',
-        size: (3 * width) / 4,
-        title: 'Presentation',
-      },
-    };
-
-    setQrElement(await agentContext.agent.qrURIElement(uriElementArgs));
-  };
-
-  useEffect(() => {
-    void delegate();
-  }, []);
-
   return (
     <SSIContainer>
       <QRPresentationViewContainer>
-        <QRContainer>{qrElement}</QRContainer>
+        <QRContainer>
+          <QRCode
+            title={'Presentation'}
+            value={'some_uri_content'} // TODO Replace this with some result once back end for holder
+            size={(3 * width) / 4}
+          />
+        </QRContainer>
       </QRPresentationViewContainer>
     </SSIContainer>
   );
