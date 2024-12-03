73306b697de457f231bfd69418b3e93079ff83f1
John Melati <github@jcmelati.com>
2024-12-03 03:17:26 +0100

    chore: creates keys in kms azure with correct name

--- src/services/identityService.ts
+++ src/services/identityService.ts
@@ -27,7 +27,7 @@ export const createIdentifier = async (args: ICreateIdentifierArgs, context: IRe
   const identifier = await context.agent.didManagerCreate({
     kms: args?.createOpts?.kms ?? KeyManagementSystemEnum.AZURE_KEY_VAULT_REST,
     ...(args?.method && {provider: `${DID_PREFIX}:${args?.method}`}),
-    alias: args?.createOpts?.alias ?? `${IdentifierAliasEnum.PRIMARY}-${args?.method}-${args?.createOpts?.options?.type}-${new Date().toUTCString()}`,
+    alias: args?.createOpts?.alias ?? `${IdentifierAliasEnum.PRIMARY}-${args?.method}-${args?.createOpts?.options?.type}-${new Date().getTime()}`,
     options: args?.createOpts?.options,
   });
 
