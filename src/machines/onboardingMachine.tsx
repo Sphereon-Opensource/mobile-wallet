255c1e2fc9cb016d7b3e4d9c6455c7f818e8ea2f
sanderPostma <spostma@sphereon.com>
2024-12-05 12:09:18 +0100

    chore: eSim back fixes again

--- src/machines/onboardingMachine.tsx
+++ src/machines/onboardingMachine.tsx
@@ -264,6 +264,11 @@ const states: OnboardingStatesConfig = {
       src: OnboardingMachineServices.activateESim,
       onDone: [
         {
+          cond: (context: OnboardingMachineContext): boolean => context.esimActivationAborted === true,
+          target: OnboardingMachineStateType.acceptTermsAndPrivacy,
+          actions: assign({currentStep: OnboardingMachineStep.SECURE_WALLET})
+        },
+        {
           cond: OnboardingMachineGuards.isSkipImport,
           target: OnboardingMachineStateType.setupWallet,
           actions: assign({currentStep: OnboardingMachineStep.FINAL})
--- src/services/machines/onboardingMachineService.ts
+++ src/services/machines/onboardingMachineService.ts
@@ -191,7 +191,7 @@ export const storeCredentialBranding = async (context: Pick<OnboardingMachineCon
 export const activateESim = async (
   context: OnboardingMachineContext,
   event: OnboardingMachineEventTypes,
-): Promise<ESIMActivationMachineContext> => {
+): Promise<void> => {
   console.log('Starting activateESim service with context:', context);
 
   return new Promise((resolve, reject) => {
@@ -206,12 +206,18 @@ export const activateESim = async (
         resolve(doneEvent.data);
       });
       esimMachineInstance.onTransition((state) => {
-        console.log('ESIMActivationMachine state transition:', state.value);
+        console.log('OnboardingMachine<->ESIMActivationMachine state transition:', state.value);
         if (state.matches('success')) {
           console.log('ESIMActivationMachine ended successfully');
-          resolve(state.context);
-        } else if (state.matches('handleError') || state.matches('error')) {
+          context.esimActivationAborted = false
+          resolve();
+        } else if (state.matches('abort')) {
+          context.esimActivationAborted = true
+          resolve();
+        } 
+        else if (state.matches('handleError') || state.matches('error')) {
           console.error('ESIMActivationMachine error:', state.context.error);
+          context.esimActivationAborted = false
           reject(state.context.error);
         }
       });
