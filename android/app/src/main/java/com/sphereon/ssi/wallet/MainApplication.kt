40ea45ccfd80dfe07bf744a8991f9d40af53bd53
sanderPostma <spostma@sphereon.com>
2025-12-05 00:16:54 +0100

    chore: reapplied musap & ausweis

--- android/app/src/main/java/com/sphereon/ssi/wallet/MainApplication.kt
+++ android/app/src/main/java/com/sphereon/ssi/wallet/MainApplication.kt
@@ -1,31 +1,36 @@
 package com.sphereon.ssi.wallet
-import id.animo.ausweissdk.AusweisSdkUtils
 
 import android.app.Application
 import android.content.res.Configuration
-
+import android.util.Log
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.ReactHost
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
-
+import com.sphereon.musap.MusapBridgeAndroid
+import com.sphereon.musap.MusapPackage
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
+import id.animo.ausweissdk.AusweisSdkUtils
 
+@OptIn(ExperimentalStdlibApi::class)
+@kotlinx.coroutines.ExperimentalCoroutinesApi
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
         this,
         object : DefaultReactNativeHost(this) {
-          override fun getPackages(): List<ReactPackage> {
-            val packages = PackageList(this).packages
-            // Packages that cannot be autolinked yet can be added manually here, for example:
-            // packages.add(MyReactNativePackage())
+                override fun getPackages(): kotlin.collections.List<ReactPackage> {
+                    val packages = PackageList(this).packages.toMutableList()
+                    try {
+                        packages.add(MusapPackage())
+                    } catch (e: Exception) {
+                        Log.e("MWALL", "Failed to add MusapPackage", e)
+                    }
             return packages
           }
 
@@ -44,7 +49,15 @@ class MainApplication : Application(), ReactApplication {
   override fun onCreate() {
     super.onCreate()
 if (AusweisSdkUtils.isAA2Process(this)) return
-    SoLoader.init(this, OpenSourceMergedSoMapping)
+
+        try {
+            MusapBridgeAndroid.initializeMusap(this)
+        } catch (e: Throwable) {
+            Log.e("MWALL", "init failed", e) // To logcat
+            throw e
+        }
+
+        SoLoader.init(this,  /* native exopackage */false)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
